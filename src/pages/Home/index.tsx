6924b97e9d8c86a326b6da7d5461ac284e044681
Alexsandro <sandro.alex_@hotmail.com>
2020-07-31 23:36:38 -0300

    Initial commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+
+import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Header />
+
+      <div>Conteudo</div>
+    </Container>
+  );
+};
+
+export default Home;
